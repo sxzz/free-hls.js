3d3a777a2e5fb581690ac7d0c51bb01082138ec0
Kevin Deng <sxzz@sxzz.moe>
2020-02-26 04:31:02 +0800

    feat: support multiple uploader

--- upload.js
+++ upload.js
@@ -10,6 +10,7 @@ const glob = require("glob");
 const FormData = require("form-data");
 const PromisePool = require("es6-promise-pool");
 require("dotenv").config();
+const uploader = require("./uploader/" + process.env.UPLOAD_DRIVE);
 
 function bit_rate(file) {
   return parseInt(
@@ -49,41 +50,6 @@ async function command_generator(file) {
   }
 
   return ` -i ${file} -vcodec ${vcodec} -acodec aac -map 0 -f segment -segment_list out.m3u8 ${sub} out%05d.ts`;
-}
-
-async function upload_yuque(file) {
-  const data = new FormData();
-  data.append("file", fs.createReadStream(file), {
-    filename: "image.png",
-    contentType: "image/png"
-  });
-
-  let res;
-  try {
-    res = await axios({
-      url: `https://www.yuque.com/api/upload/attach?ctoken=${process.env.YUQUE_CTOKEN}`,
-      method: "POST",
-      data,
-      headers: {
-        Referer: "https://www.yuque.com/yuque/topics/new",
-        Cookie: `ctoken=${process.env.YUQUE_CTOKEN}; _yuque_session=${process.env.YUQUE_SESSION}`,
-        ...data.getHeaders()
-      }
-    });
-  } catch (err) {
-    if (err.response.status === 403) {
-      console.warn("⚠️ Cookie 已过期");
-      process.exit();
-    } else {
-      return Promise.reject(err);
-    }
-  }
-
-  if (res.data.data && res.data.data.url) {
-    return { filename: path.basename(file), url: res.data.data.url };
-  } else {
-    return null;
-  }
 }
 
 async function publish(code, title = null) {
@@ -126,7 +92,7 @@ async function main() {
 
   const generatePromises = function*() {
     for (const ts_file of ts_files) {
-      yield upload_yuque(ts_file);
+      yield uploader(ts_file);
     }
   };
   const concurrency = 10;
